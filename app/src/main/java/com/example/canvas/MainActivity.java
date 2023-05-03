3b959ceb33d7c6038af89a4476f03af52ad309bf
farhanakmals <shaleh.farhan@gmail.com>
2023-05-03 16:02:09 +0700

    first commit

--- app/src/main/java/com/example/canvas/MainActivity.java
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.core.content.res.ResourcesCompat;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Rect mBounds = new Rect();
 
     // untuk batasan batasan gambarnya
-    private static final int OFFSET = 12;
+    private static final int OFFSET = 120;
     private int mOffset = OFFSET;
     private static final  int MULTIPLIER = 100;
 
@@ -84,14 +86,67 @@ public class MainActivity extends AppCompatActivity {
                 mPaint.setColor(mColorCircle - MULTIPLIER * mOffset);
                 mCanvas.drawCircle(halfWidth,halfHeight, halfHeight/3, mPaint);
 
+                // menambahan text
                 String text = getString(R.string.done);
 
+                // mencari panjang dari text
                 mPaintText.getTextBounds(text, 0, text.length(), mBounds);
 
+                // memindahkan text ke tengah
                 int x = halfWidth - mBounds.centerX();
                 int y =  halfHeight- mBounds.centerY();
                 mCanvas.drawText(text, x, y, mPaintText);
 
+                mOffset += OFFSET;
+
+                // membuat segitiga
+//                mPaint.setColor(mColorBackground - MULTIPLIER * mOffset);
+//                Point a = new Point(halfWidth - 50, halfHeight - 50);
+//                Point b = new Point(halfWidth + 50, halfHeight - 50);
+//                Point c = new Point(halfWidth, halfHeight + 250);
+//
+//                Path path = new Path();
+//                path.setFillType(Path.FillType.EVEN_ODD);
+//                path.lineTo(a.x, a.y);
+//                path.lineTo(b.x, b.y);
+//                path.lineTo(c.x, c.y);
+//                path.lineTo(a.x, a.y);
+//                path.close();
+//
+//                mCanvas.drawPath(path, mPaint);
+//                mOffset += OFFSET;
+
+                // tugas
+                mPaint.setColor(mColorBackground - MULTIPLIER * mOffset);
+                Point a = new Point(halfWidth - 310, halfHeight - 50);
+                Point b = new Point(halfWidth - 85, halfHeight - 70);
+                Point c = new Point(halfWidth, halfHeight - 310);
+                Point d = new Point(halfWidth + 85, halfHeight - 70);
+                Point e = new Point(halfWidth + 310, halfHeight - 50);
+                Point f = new Point(halfWidth + 100, halfHeight + 50);
+                Point g = new Point(halfWidth + 170, halfHeight + 270);
+                Point h = new Point(halfWidth, halfHeight + 100);
+                Point i = new Point(halfWidth - 170, halfHeight + 270);
+                Point j = new Point(halfWidth - 100, halfHeight + 40);
+
+                Path path = new Path();
+                path.setFillType(Path.FillType.EVEN_ODD);
+                path.lineTo(a.x, a.y);
+                path.lineTo(b.x, b.y);
+                path.lineTo(c.x, c.y);
+                path.lineTo(d.x, d.y);
+                path.lineTo(e.x, e.y);
+                path.lineTo(f.x, f.y);
+                path.lineTo(g.x, g.y);
+                path.lineTo(h.x, h.y);
+                path.lineTo(i.x, i.y);
+                path.lineTo(j.x, j.y);
+
+                path.lineTo(a.x, a.y);
+
+                path.close();
+
+                mCanvas.drawPath(path, mPaint);
                 mOffset += OFFSET;
             }
 
